52d41bbb5529a2ea90d464396c6597bcd63d0a57
RenanArantes <malevo18@gmail.com>
2021-11-29 14:20:56 -0300

    typeAlias-vs-Interface

---
+++ sumnumbers/interfaces-vs.ts
@@ -0,0 +1,58 @@
+//interfaces
+
+//definicao
+interface Gamee {
+    title: string;
+}
+
+interface DLC {
+    extra: string;
+}
+
+//intersecao | extend
+interface GameCollecion extends Gamee , DLC {}
+
+//implements
+class CreateGamee implements GameCollecion {
+    title: string;
+    extra: string;
+
+    constructor(t: string, e: string) {
+        this.title = t;
+        this.extra = e;
+    }
+}
+
+//declarar funcao
+interface getSimilars{
+    (title: string): void;
+}
+
+//----------- DIFERENCAS -----------------//
+//nao permite declarar tipos primitivos
+interface ID extends number {}
+
+//nao consegue reproduzir o comportamento da tupla
+interface Tuple {
+    0: number;
+    1: number;
+}
+
+[1, 2, 3] as Tuple;
+
+//pode ter multiplas declaracoes e ele une todas de mesmo nome
+interface JQuery {
+    a: string;
+}
+
+interface JQuery {
+    b: string;
+}
+
+const object: JQuery = {
+    a: "asda",
+    b: "qweq"
+}
+
+//indicado para a criacao de libs, projetos extensiveis e reutilizaveis, sao extensiveis
+//indicado com POO
---
+++ sumnumbers/types-vs.ts
@@ -0,0 +1,44 @@
+//Type Alias
+
+//definicao
+type Jogo = {
+    titulo: string;
+}
+
+type Expansao = {
+    extra: string;
+}
+
+//intersection
+type ColecaoDeJogos = Jogo & Expansao;
+
+//implements
+class CriarJogo implements ColecaoDeJogos {
+    titulo: string;
+    extra: string;
+
+    constructor (t: string, e: string) {
+        this.titulo = t;
+        this.extra = e;
+    }
+}
+
+//declarar funcao
+type getSimilares = (titulo: string) => void;
+
+//----------- DIFERENCAS -----------------//
+//permite declarar tipos primitivos
+type Identidade = string | number;
+
+//pode declarar tuplas
+type Tupla = [number, number];
+
+[1, 2] as Tupla;
+
+//Apenas uma declaracao por escopo
+type JQueryy = { a: string };
+type JQueryy = { b: string };
+
+//mais recomendado no geral
+//na maioria das vezes
+//react - props
